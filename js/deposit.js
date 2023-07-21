880bd47d25d0c29dd16403b259c3a9004c3f32b0
sajalsa86e <sajalsm86@gmail.com>
2023-07-21 23:34:59 +0600

    last update

--- js/deposit.js
+++ js/deposit.js
@@ -1,33 +1,29 @@
-//deposit
-//step -1
+//step-1
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    //step -2
+
+    //step-2
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
-    //step 7
+    //step-3
     depositField.value = '';
     //problem fix
     if (isNaN(newDepositAmount)) {
         alert('Please Provide a valid Number');
         return;
     }
-    //step -3
+    //step-4
     const depositElement = document.getElementById('deposit-element');
     const previousDepositString = depositElement.innerText;
     const previousDeposit = parseFloat(previousDepositString);
-    //step -4
+    //step-5
     const currentTotalDeposit = previousDeposit + newDepositAmount;
     depositElement.innerText = currentTotalDeposit;
-    //step -5
-    // added Total balance
+    //step-6
     const balanceElement = document.getElementById('balance-element');
     const previousBalancetString = balanceElement.innerText;
     const previousBalance = parseFloat(previousBalancetString);
-    //step -6
-    //sum total deposit and total balance
-    const totalBlance = previousBalance + newDepositAmount;
-    balanceElement.innerText = totalBlance;
-
-
+    //step-7
+    const remaingBlance = previousBalance + newDepositAmount;
+    balanceElement.innerText = remaingBlance;
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,45 +1,33 @@
-//withdraw 
 //step-1
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    ///step-2
+    //step-2
     const withdrawField = document.getElementById('withdraw-field');
-    const newWithdrawAmoutString = withdrawField.value;
-    const newWithdrawAmout = parseFloat(newWithdrawAmoutString);
-    //step-7
+    const newWithdrawString = withdrawField.value;
+    const newWithdrawAmout = parseFloat(newWithdrawString);
+    //step-3
     withdrawField.value = '';
-
     //problem fix
     if (isNaN(newWithdrawAmout)) {
-        alert('Please Provide a valid Number');
+        alert('Please Provide a valid number');
         return;
     };
-
-    //step-3
+    //step-4
     const withdrawElement = document.getElementById('withdraw-element');
     const previouWithdrawString = withdrawElement.innerText;
     const previousWithdraw = parseFloat(previouWithdrawString);
-
-
-    //decrcase withdraw from total balance
-    //step-5
+    //step-6
     const balanceElement = document.getElementById('balance-element');
     const previousBalancetString = balanceElement.innerText;
     const previousBalance = parseFloat(previousBalancetString);
-
-
-
     //problem fix
     if (newWithdrawAmout > previousBalance) {
         alert("You don't have Enough Balance");
         return;
     }
-
-    //step-4
+    //step-5
     const currentTotalWithdraw = previousWithdraw + newWithdrawAmout;
     withdrawElement.innerText = currentTotalWithdraw;
-
-    //step-6
-    const totalBlance = previousBalance - newWithdrawAmout;
-    balanceElement.innerText = totalBlance;
-
+    //step-7
+    const remaingBlance = previousBalance - newWithdrawAmout;
+    balanceElement.innerText = remaingBlance;
 });
